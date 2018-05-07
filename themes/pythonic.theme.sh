37d7589c07ad3edc25e3bf483cbf4cc8b483128b
DevThiago <devthiago.py@gmail.com>
2018-05-07 00:56:01 -0300

    Update pythonic theme

--- themes/pythonic.theme.sh
+++ themes/pythonic.theme.sh
@@ -1,14 +1,15 @@
-# Load git module
+# Load module
 . ~/.prompt_maker/modules/git
 . ~/.prompt_maker/modules/custom_pwd
-# Colors that not change with pywal
-reset="\001$(tput sgr0)\002"
-bold="\001$(tput bold)\002"
-red="\001\033[38;5;160m\002"
-green="\001\033[38;5;82m\002"
-yellow="\001\033[38;5;226m\002"
-blue="\001\033[38;5;33m\002"
-purple="\001\033[38;5;177m\002"
-white="\001\033[38;5;255m\002"
-
-# Prompt  ~ < master >>>
+. ~/.prompt_maker/var/colors
+# Git stuff
+GIT_STATUS_DIRTY=""
+GIT_BRANCH_COLOR_CLEAN="${b_green}"
+GIT_BRANCH_COLOR_DIRTY="${b_red}"
+GIT_PREFIX=""
+GIT_SUFFIX="${n_white} ${reset}"
+# Custom PWD stuff 
+. ~/.prompt_maker/var/custom_pwd
+CUSTOM_PWD_COLOR="${n_blue}"
+# Prompt  branch ~/ >>>
+PM_PROMPT="${reset}\$(git_prompt)\$(custom_pwd) ${b_yellow}>${b_green}>${b_blue}> ${reset}"
